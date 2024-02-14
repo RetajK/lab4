5a99d91d737f27758ffb81e025a64d561282c584
RetajK <151464985+RetajK@users.noreply.github.com>
2024-02-14 14:24:59 +0300

    Retaj Alghamdi and Layan Almousa

--- app/src/main/java/com/example/lab4/SecondActivity.java
+++ app/src/main/java/com/example/lab4/SecondActivity.java
@@ -2,13 +2,25 @@ package com.example.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+     TextView receiver_msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        TextView receiver_msg = findViewById(R.id.received_value_id);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("message_key");
+        receiver_msg.setText("Hello, " +str +" !");
+
+
+
     }
 }
