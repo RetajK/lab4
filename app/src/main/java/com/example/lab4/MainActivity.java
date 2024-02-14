6692e65736bea3fe7b91280b93dbe695b7ced4b8
RetajK <151464985+RetajK@users.noreply.github.com>
2024-02-14 13:47:21 +0300

    Initial committ

---
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button ;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button= findViewById(R.id.send_button_id);
+        editText= findViewById(R.id.send_text_id);
+    }
+}
